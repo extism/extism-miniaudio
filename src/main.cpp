176008725cca18c037108e7c3e50ba3ef9605faf
Gavin Hayes <gavin@dylibso.com>
2023-12-07 21:47:13 -0500

    cleanup prior to adding data soruce

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,72 @@
 #include <iostream>
+#include <optional>
 #include "extism.hpp"
 #define MINIAUDIO_IMPLEMENTATION
 #include "miniaudio.h"
-int main(int argc, char *argv[])
+
+static const std::optional<extism::Buffer> download_audio(extism::Plugin &plugin, std::string src)
 {
-    if (argc < 2)
-    {
-        std::cerr << "A URL to an audio file must be provided!" << std::endl;
-        return 1;
-    }
-    const std::string code = "wasm-src/plugin.wasm";
-    extism::Manifest manifest = extism::Manifest::wasmPath(code);
-    manifest.allowHost("SET_ME");
-    extism::Plugin plugin(manifest, true);
-    extism::Buffer buf = plugin.call("count_vowels", "this is a test");
-    std::cout << static_cast<std::string>(buf) << std::endl;
     try
     {
-        extism::Buffer buf2 = plugin.call("download_audio", argv[1]);
-        //std::cout << static_cast<std::string>(buf2) << std::endl;
-        ma_decoder decoder;
-        if (MA_SUCCESS != ma_decoder_init_memory(buf2.data, buf2.length, NULL, &decoder))
-        {
-            std::cerr << "error initializing decoder" << std::endl;
-            return 1;
-        }
-        ma_engine engine;
-        if (MA_SUCCESS != ma_engine_init(NULL, &engine))
-        {
-            std::cerr << "error initializing engine" << std::endl;
-            return 1;
-        }
-        ma_engine_start(&engine);
-        ma_sound sound;
-        ma_sound_init_from_data_source(&engine, &decoder, 0, NULL, &sound);
-        ma_sound_start(&sound);
-        while (1)
-            ;
+        extism::Buffer buf = plugin.call("download_audio", std::move(src));
+        return buf;
     }
-    catch (...)
+    catch (const extism::Error &)
     {
-        std::cerr << "plugin call failed" << std::endl;
+        return {};
     }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        std::cerr << "A URL to an audio file and an allowed host must be provided!" << std::endl;
+        return 1;
+    }
+    const std::string code("wasm-src/plugin.wasm");
+    extism::Manifest manifest = extism::Manifest::wasmPath(code);
+    manifest.allowHost(argv[2]);
+    extism::Plugin plugin(manifest, true);
+    const auto maybeBuf = download_audio(plugin, argv[1]);
+    if (!maybeBuf)
+    {
+        std::cerr << "Failed to download audio!" << std::endl;
+        return 1;
+    }
+    const extism::Buffer &buf = *maybeBuf;
+    ma_decoder decoder;
+    if (MA_SUCCESS != ma_decoder_init_memory(buf.data, buf.length, NULL, &decoder))
+    {
+        std::cerr << "error initializing decoder" << std::endl;
+        return 1;
+    }
+    ma_engine engine;
+    if (MA_SUCCESS != ma_engine_init(NULL, &engine))
+    {
+        std::cerr << "error initializing engine" << std::endl;
+        return 1;
+    }
+    if (MA_SUCCESS != ma_engine_start(&engine))
+    {
+        std::cerr << "error starting engine" << std::endl;
+        return 1;
+    }
+    ma_sound sound;
+    if (MA_SUCCESS != ma_sound_init_from_data_source(&engine, &decoder, 0, NULL, &sound))
+    {
+        std::cerr << "error initializing sound" << std::endl;
+        return 1;
+    }
+    if (MA_SUCCESS != ma_sound_start(&sound))
+    {
+        std::cerr << "error starting sound" << std::endl;
+        return 1;
+    }
+    while (!ma_sound_at_end(&sound))
+    {
+    }
+    ma_sound_uninit(&sound);
+    ma_engine_uninit(&engine);
+    ma_decoder_uninit(&decoder);
 }
--- wasm-src/plugin.c
+++ wasm-src/plugin.c
@@ -7,31 +7,6 @@
 #include "miniaudio.h"
 #include "extism-pdk.h"
 
-EXTISM_EXPORT_AS("count_vowels")
-int32_t count_vowels(void)
-{
-  uint64_t count = 0;
-  uint8_t ch = 0;
-  uint64_t length = extism_input_length();
-
-  for (uint64_t i = 0; i < length; i++)
-  {
-    ch = extism_input_load_u8(i);
-    count += (ch == 'A') + (ch == 'a') + (ch == 'E') + (ch == 'e') +
-             (ch == 'I') + (ch == 'i') + (ch == 'O') + (ch == 'o') +
-             (ch == 'U') + (ch == 'u');
-  }
-
-  char out[128];
-  int n = snprintf(out, 128, "{\"count\": %llu}", count);
-
-  uint64_t offs_ = extism_alloc(n);
-  extism_store(offs_, (const uint8_t *)out, n);
-  extism_output_set(offs_, n);
-
-  return 0;
-}
-
 static char *extism_input_load_string(void)
 {
   uint64_t buf_size = extism_input_length() + 1;
@@ -63,21 +38,13 @@ int32_t EXTISM_EXPORTED_FUNCTION(download_audio)
   }
   mempcpy(mempcpy(mempcpy(reqStr, get_req_prefix, sizeof(get_req_prefix) - 1), url, url_len), get_req_suffix, sizeof(get_req_suffix));
   free(url);
-
-  //const char *reqStr = "{\
-  //  \"method\": \"GET\",\
-  //  \"url\": \"https://jsonplaceholder.typicode.com/todos/1\"\
-  //}";
-
   ExtismPointer req = extism_alloc_string(reqStr, strlen(reqStr));
   free(reqStr);
   ExtismPointer res = extism_http_request(req, 0);
-
   if (extism_http_status_code() != 200)
   {
     return -1;
   }
-
   extism_output_set(res, extism_length(res));
   return 0;
 }
